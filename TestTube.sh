e4ddb2953d4921b5787f0cff082ab454189fb874
Richard42Graham <up628887@myport.ac.uk>
2019-10-29 20:40:14 +0000

    added more test code, and documentation

--- TestTube.sh
+++ TestTube.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 echo "setting up IO chip"
-i2cwrite -y 1 0x20 0x00 0x00 # setup port A as all outputs
-i2cwrite -y 1 0x20 0x01 0x00 # setup port B as all outputs
+i2cset -y 1 0x20 0x00 0x00 # setup port A as all outputs
+i2cset -y 1 0x20 0x01 0x00 # setup port B as all outputs
 
 echo "turning on HV"
-i2cwrite -y 1 0x20 0x13 0x16 # set pin A4 HIGH to enable HV
+i2cset -y 1 0x20 0x13 0x16 # set pin A4 HIGH to enable HV
+
+
+
+i2cset -y 1 0x40 0x00 0x00 # set mode 1
+
+### https://www.raspberrypi.org/forums/viewtopic.php?t=9007 ### 
+
+
+
 
 echo "setting digit"
-i2cwrite -y 1 0x40 0x?? 0x01 ?? # set first (tube closest to power in) to a digit
+i2cset -y 1 0x40 0x?? 0x01 ?? # set first (tube closest to power in) to a digit
 # set it to a breightness,
 
 # turn on enbale pin to show tube lighting up. 
