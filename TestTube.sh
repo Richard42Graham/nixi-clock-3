5a4ef6808d6b30f53fcba3f2ddd173183ee85ccb
Richard42Graham <up628887@myport.ac.uk>
2019-10-31 13:17:10 +0000

    added more test script to test every digit on every tube

--- TestTube.sh
+++ TestTube.sh
@@ -5,27 +5,96 @@ i2cset -y 1 0x20 0x00 0x00 # setup port A as all outputs
 i2cset -y 1 0x20 0x01 0x00 # setup port B as all outputs
 
 echo "turning on HV"
-i2cset -y 1 0x20 0x14 0x16 # set pin A4 HIGH to enable HV
-
-
+i2cset -y 1 0x20 0x14 0x00 # set pin A4 HIGH to enable HV and enable ouput on PWM chips
 
 i2cset -y 1 0x40 0x00 0x00 # set mode 1
-
-i2cset -y 2 0x40 0x06 0
-i2cset -y 2 0x40 0x07 0
+i2cset -y 1 0x40 0x06 0
+i2cset -y 1 0x40 0x07 0
 # i2cset -y 2 0x40 0x08 88
-i2cset -y 2 0x40 0x09 2 # 2 is the var
+i2cset -y 1 0x40 0x09 2 # 2 is the var
 ### https://www.raspberrypi.org/forums/viewtopic.php?t=9007 ### 
+sleep 0.5
+
+for counter in 1 2 3 4 5 6 7 8 9
+do
+echo $counter
+i2cset -y 1 0x40 0x09 $counter 
+sleep 0.3
+done
+
+for i in 10 9 8 7 6 5 4 3 2 1  0
+do 
+echo $i
+i2cset -y 1 0x40 0x09 $i
+sleep 0.3  
+done 
 
 
+sleep 1
+echo "changing digit"
 
 
-echo "setting digit"
-i2cset -y 1 0x40 0x?? 0x01 ?? # set first (tube closest to power in) to a digit
-# set it to a breightness,
+i2cset -y 1 0x40 0x0A 0		# setup PWM for digit 0
+i2cset -y 1 0x40 0x0B 0
+i2cset -y 1 0x40 0x0C 0
 
-# turn on enbale pin to show tube lighting up. 
+for counter in 1 2 3 4 5 6 7 8 9
+do
+echo $counter
+i2cset -y 1 0x40 0x0D $counter
+sleep 0.3
+done
+
+for i in 10 9 8 7 6 5 4 3 2 1  0
+do
+echo $i
+i2cset -y 1 0x40 0x0D $i
+sleep 0.3
+done
 
 
+sleep 1
+echo "changing digit"
 
-# maybe do a dimming loop between some digit to test them
+
+i2cset -y 1 0x40 0x0E 0         # setup PWM for digit 0
+i2cset -y 1 0x40 0x0F 0
+i2cset -y 1 0x40 0x10 0
+
+for counter in 1 2 3 4 5 6 7 8 9
+do
+echo $counter
+i2cset -y 1 0x40 0x11 $counter
+sleep 0.3
+done
+
+for i in 10 9 8 7 6 5 4 3 2 1  0
+do
+echo $i
+i2cset -y 1 0x40 0x11 $i
+sleep 0.3
+done
+
+
+sleep 1
+echo "changing digit"
+
+
+i2cset -y 1 0x40 0x12 0         # setup PWM for digit 0
+i2cset -y 1 0x40 0x13 0
+i2cset -y 1 0x40 0x14 0
+
+for counter in 1 2 3 4 5 6 7 8 9
+do
+echo $counter
+i2cset -y 1 0x40 0x15 $counter
+sleep 0.3
+done
+
+for i in 10 9 8 7 6 5 4 3 2 1  0
+do
+echo $i
+i2cset -y 1 0x40 0x15 $i
+sleep 0.3
+done
+
