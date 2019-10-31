60209d60d99c398fdf0739a48bc7558d16a1812f
Richard42Graham <up628887@myport.ac.uk>
2019-10-31 11:58:00 +0000

    finaly got Test tubes working a light :P

--- TestTube.sh
+++ TestTube.sh
@@ -5,12 +5,16 @@ i2cset -y 1 0x20 0x00 0x00 # setup port A as all outputs
 i2cset -y 1 0x20 0x01 0x00 # setup port B as all outputs
 
 echo "turning on HV"
-i2cset -y 1 0x20 0x13 0x16 # set pin A4 HIGH to enable HV
+i2cset -y 1 0x20 0x14 0x16 # set pin A4 HIGH to enable HV
 
 
 
 i2cset -y 1 0x40 0x00 0x00 # set mode 1
 
+i2cset -y 2 0x40 0x06 0
+i2cset -y 2 0x40 0x07 0
+# i2cset -y 2 0x40 0x08 88
+i2cset -y 2 0x40 0x09 2 # 2 is the var
 ### https://www.raspberrypi.org/forums/viewtopic.php?t=9007 ### 
 
 
