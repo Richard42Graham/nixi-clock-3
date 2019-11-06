f9368b0945bb7b356d12c26ebf3d2162d90f9b2e
Richard42Graham <up628887@myport.ac.uk>
2019-11-06 12:36:13 +0000

    started work on first test C file

--- adcTEST.sh
+++ adcTEST.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-addr = 0x55
+addr=0x55
 
 i2cset -y 1 $addr 0x02 0x20 # set automatic convertion mode
 sleep 0.5
 
-var1 = i2cget -y 1 $addr 0x00 # MSB
-var2 = i2cget -y 1 $addr 0x01 # LSB
+var1=i2cget -y 1 $addr 0x00 # MSB
+var2=i2cget -y 1 $addr 0x01 # LSB
 
 oupput =((($var1 & 0x0F)*256) + $var2)
 
