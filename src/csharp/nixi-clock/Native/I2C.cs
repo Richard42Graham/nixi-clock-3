fd405ec703fb226dcf8973378c9e4f4fe52c8271
Jens Larsen <mr.javawolf@gmail.com>
2019-11-17 22:46:59 +0100

    Moved: c code into src/c/ which makes space for src/csharp folder

---
+++ src/csharp/nixi-clock/Native/I2C.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace nixi_clock.Native
+{
+    public static class I2C
+    {
+
+        [DllImport("a.out")]
+        public static extern int open_bus(char[] bus);
+
+        [DllImport("a.out")]
+        public static extern int read_register(int fd, byte device_address, byte reg, out byte output);
+
+        [DllImport("a.out")]
+        public static extern int write_register(int fd, byte device_address, byte reg, byte input);
+
+        [DllImport("a.out")]
+        public static extern int close_bus(int fd);
+
+    }
+}
---
+++ src/csharp/nixi-clock/Program.cs
@@ -0,0 +1,44 @@
+﻿using nixi_clock.Native;
+using System;
+using System.Threading;
+
+namespace nixi_clock
+{
+    class Program
+    {
+        const string BUS = "/dev/i2c-0";
+        static void Main(string[] args)
+        {
+            //Opens bus
+            int fd;
+            if ((fd = I2C.open_bus(BUS.ToCharArray())) < 0)
+            {
+                Console.WriteLine("Failed to open bus! %d\n", fd);
+                return;
+            }
+            if (Tubes.init_tubes(fd) != 0)
+            {
+                Console.WriteLine("Failed to init the tubes! %d\n", fd);
+                return;
+            }
+            int brightness, tube, digit = 0;
+            for (tube = 0; tube < 7; tube++)
+            {
+                for (digit = 0; digit < 12; digit++)
+                {
+                    for (brightness = 1; brightness <= 0x09; brightness++)
+                    {
+                        Tubes.set_tube(fd, tube, digit, (byte)brightness);
+                        Thread.Sleep(33);
+                    }
+                    for (brightness = 0x08; brightness >= 0; brightness--)
+                    {
+                        Tubes.set_tube(fd, tube, digit, (byte)brightness);
+                        Thread.Sleep(33);
+                    }
+                }
+            }
+            I2C.close_bus(fd);
+        }
+    }
+}
