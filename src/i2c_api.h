997969dfcb5b87a55d837424670661263bfe7aae
Jens Larsen <mr.javawolf@gmail.com>
2019-11-17 18:05:34 +0100

    Added: All functions in the header files are now external callable

--- src/i2c_api.h
+++ src/i2c_api.h
@@ -2,13 +2,14 @@
 #ifndef I2C_API_H_INCLUDED
 #define I2C_API_H_INCLUDED
 #include <sys/types.h>
+#include "export.h"
 
-int open_bus(char bus[]);
+EXPORT int open_bus(char bus[]);
 
-int read_register(int fd, u_int8_t device_address, u_int8_t reg, char* output);
+EXPORT int read_register(int fd, u_int8_t device_address, u_int8_t reg, char* output);
 
-int write_register(int fd, u_int8_t device_address, u_int8_t reg, u_int8_t input);
+EXPORT int write_register(int fd, u_int8_t device_address, u_int8_t reg, u_int8_t input);
 
-int close_bus(int fd);
+EXPORT int close_bus(int fd);
 
 #endif
--- src/tube_api.h
+++ src/tube_api.h
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef TUBE_API_H_INCLUDED
 #define TUBE_API_H_INCLUDED
+#include "export.h"
 #include <sys/types.h>
 
 // Info
@@ -11,10 +12,8 @@
 #define TUBE_DOES_NOT_EXIST 42
 #define DIGIT_DOES_NOT_EXIST 7
 
-int init_tubes(int fd);
+EXPORT int init_tubes(int fd);
 
-int set_tube(int fd, int tupe, int digit, u_int8_t brightness);
-
-
+EXPORT int set_tube(int fd, int tupe, int digit, u_int8_t brightness);
 
 #endif
