d5ab7e682951875e3941c3b152d54b970dca18cd
Jens Larsen <mr.javawolf@gmail.com>
2019-11-17 13:55:35 +0100

    Added: open/close api

--- api.c
+++ api.c
@@ -1,5 +1,30 @@
 #include "api.h"
 
+int open(int fd, char[] bus){
+     int fd;
+    if ((fd = open(bus, O_RDWR)) < 0)
+    {
+        //Failed to open
+        return fd;
+    }
+    init(fd);
+}
+
+int init(int fd){
+    int i=0;
+        for (i = 0; i < sizeof(chips)/sizeof(char); i++)
+        {
+            if(write_reg(fd,chips[i],0x00,0x00)<0){
+                return 1;
+            }
+        }
+
+}
+
+int close(int fd){
+    return close(fd);
+}
+
 int read_reg(int fd, u_int8_t device_address, u_int8_t reg, char *output)
 {
     ioctl(fd, I2C_SLAVE, device_address);
@@ -20,6 +45,7 @@ int read_reg(int fd, u_int8_t device_address, u_int8_t reg, char *output)
          .nmsgs = 2}};
     return ioctl(fd, I2C_RDWR, &msgset);
 }
+
 int write_reg(int fd, u_int8_t device_address, u_int8_t reg, char input)
 {
     ioctl(fd, I2C_SLAVE, device_address);
@@ -36,16 +62,7 @@ int write_reg(int fd, u_int8_t device_address, u_int8_t reg, char input)
     return ioctl(fd, I2C_RDWR, &msgset);
 }
 
-int init(int fd){
-    int i=0;
-        for (i = 0; i < sizeof(chips)/sizeof(char); i++)
-        {
-            if(write_reg(fd,chips[i],0x00,0x00)<0){
-                return 1;
-            }
-        }
 
-}
 int set_digit(int fd,digit digi,char value){
     return write_reg(fd,digi.chip,digi.adress, value);
 }
--- test.c
+++ test.c
@@ -3,26 +3,19 @@
 #include <errno.h>
 #include <string.h>
 #include <math.h>
-
-#include <fcntl.h>     // open bus file
-#include <sys/ioctl.h> // to open I2C device
-#include <unistd.h>    // read/write to i2c device,
-#include <linux/i2c-dev.h>
 #include <sys/types.h>
 #include "api.c"
 #define BUS "/dev/i2c-1"
 
 int main()
 {
-
     //Opens bus
     int fd;
-    if ((fd = open(BUS, O_RDWR)) < 0)
+    if ((fd = open(BUS)) < 0)
     {
         printf("Couldn't open bus! %d\n", fd);
         return 1;
     }
-    init(fd);
     int i, tube, digi = 0;
     for (tube = 0; tube < 7; tube++)
     {
