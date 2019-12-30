a6a0d0364d3cd097ca5f09f2e9ffa7dd2287d404
Jens Larsen <mr.javawolf@gmail.com>
2019-12-30 02:51:32 +0100

    Updated: Successfully moved code to use System.Device.I2c and Iot.Device.Pwm

--- src/csharp/nixi-clock/BoardRenderer.cs
+++ src/csharp/nixi-clock/BoardRenderer.cs
@@ -1,44 +1,154 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Device.I2c;
-using Iot.Device.Pwm;
+﻿using Iot.Device.Pwm;
 using nixi_clock.Model;
+using System;
+using System.Collections.Generic;
+using System.Device.I2c;
+using System.Text;
 
 namespace nixi_clock
 {
     public class BoardRenderer
     {
-        private I2cDevice device;
-        private Pca9685 board;
-        public bool RenderToConsole { get; set; } = false;
+        public bool ToConsole { get; set; } = false;
+        private readonly Pca9685[] devices;
+        private readonly Dictionary<(int, int), (Pca9685, int)> devicesMap = new Dictionary<(int, int), (Pca9685, int)>();
 
-        public BoardRenderer()
+        private Board currentState = new Board();
+        public BoardRenderer(int busId = 0)
         {
-            //I2cDevice device;
-            //board = new Pca9685(device);
-            //board.SetDutyCycle()
+            devices = new Pca9685[6];
+            for (int i = 0; i < devices.Length; i++)
+                devices[i] = new Pca9685(I2cDevice.Create(new I2cConnectionSettings(busId, Pca9685.I2cAddressBase + i)), 150);
+            Array.ForEach(devices, d => d.SetDutyCycleAllChannels(0));
+
+            // Does not map to anything:
+            // - Device 2 Channel 0 - does not work, is it a hardware problem?
+            // - Device 4 Channel 0
+            // - Device 4 Channel 9 - 15
+            // - Device 5 Channel 7 - 10
+
+            AddMap(0, DigitType.Zero, devices[0], 1);
+            AddMap(0, DigitType.One, devices[0], 3);
+            AddMap(0, DigitType.Two, devices[0], 4);
+            AddMap(0, DigitType.Three, devices[0], 5);
+            AddMap(0, DigitType.Four, devices[0], 6);
+            AddMap(0, DigitType.Five, devices[0], 7);
+            AddMap(0, DigitType.Six, devices[0], 8);
+            AddMap(0, DigitType.Seven, devices[0], 9);
+            AddMap(0, DigitType.Eight, devices[0], 10);
+            AddMap(0, DigitType.Nine, devices[0], 11);
+            AddMap(0, DigitType.RightDot, devices[0], 0);
+            AddMap(0, DigitType.LeftDot, devices[0], 2);
+
+
+            AddMap(1, DigitType.Zero, devices[1], 6);
+            AddMap(1, DigitType.One, devices[1], 9);
+            AddMap(1, DigitType.Two, devices[0], 15);
+            AddMap(1, DigitType.Three, devices[0], 14);
+            AddMap(1, DigitType.Four, devices[0], 13);
+            AddMap(1, DigitType.Five, devices[0], 12);
+            AddMap(1, DigitType.Six, devices[1], 2);
+            AddMap(1, DigitType.Seven, devices[1], 3);
+            AddMap(1, DigitType.Eight, devices[1], 4);
+            AddMap(1, DigitType.Nine, devices[1], 5);
+            AddMap(1, DigitType.RightDot, devices[1], 7);
+            AddMap(1, DigitType.LeftDot, devices[1], 8);
+
+            AddMap(2, DigitType.Zero, devices[1], 14);
+            AddMap(2, DigitType.One, devices[1], 12);
+            AddMap(2, DigitType.Two, devices[1], 11);
+            AddMap(2, DigitType.Three, devices[1], 10);
+            AddMap(2, DigitType.Four, devices[1], 0);
+            AddMap(2, DigitType.Five, devices[1], 1);
+            AddMap(2, DigitType.Six, devices[2], 11);
+            AddMap(2, DigitType.Seven, devices[2], 10);
+            AddMap(2, DigitType.Eight, devices[2], 9);
+            AddMap(2, DigitType.Nine, devices[2], 8);
+            AddMap(2, DigitType.RightDot, devices[1], 15);
+            AddMap(2, DigitType.LeftDot, devices[1], 13);
+
+            AddMap(3, DigitType.Zero, devices[2], 1);
+            AddMap(3, DigitType.One, devices[2], 6);
+            AddMap(3, DigitType.Two, devices[2], 5);
+            AddMap(3, DigitType.Three, devices[2], 4);
+            AddMap(3, DigitType.Four, devices[2], 3);
+            AddMap(3, DigitType.Five, devices[2], 2);
+            AddMap(3, DigitType.Six, devices[2], 12);
+            AddMap(3, DigitType.Seven, devices[2], 13);
+            AddMap(3, DigitType.Eight, devices[2], 14);
+            AddMap(3, DigitType.Nine, devices[2], 15);
+            AddMap(3, DigitType.RightDot, devices[2], 0); // Does NOT work, is it a hardware problem?
+            AddMap(3, DigitType.LeftDot, devices[2], 7);
+
+            AddMap(4, DigitType.Zero, devices[3], 9);
+            AddMap(4, DigitType.One, devices[3], 8);
+            AddMap(4, DigitType.Two, devices[3], 2);
+            AddMap(4, DigitType.Three, devices[3], 7);
+            AddMap(4, DigitType.Four, devices[3], 5);
+            AddMap(4, DigitType.Five, devices[3], 4);
+            AddMap(4, DigitType.Six, devices[3], 3);
+            AddMap(4, DigitType.Seven, devices[3], 0);
+            AddMap(4, DigitType.Eight, devices[3], 11);
+            AddMap(4, DigitType.Nine, devices[3], 10);
+            AddMap(4, DigitType.RightDot, devices[3], 1);
+            AddMap(4, DigitType.LeftDot, devices[3], 6);
+
+            AddMap(5, DigitType.Zero, devices[3], 14);
+            AddMap(5, DigitType.One, devices[3], 13);
+            AddMap(5, DigitType.Two, devices[4], 2);
+            AddMap(5, DigitType.Three, devices[4], 7);
+            AddMap(5, DigitType.Four, devices[4], 5);
+            AddMap(5, DigitType.Five, devices[4], 4);
+            AddMap(5, DigitType.Six, devices[4], 3);
+            AddMap(5, DigitType.Seven, devices[3], 12);
+            AddMap(5, DigitType.Eight, devices[4], 8);
+            AddMap(5, DigitType.Nine, devices[3], 15);
+            AddMap(5, DigitType.RightDot, devices[4], 1);
+            AddMap(5, DigitType.LeftDot, devices[4], 6);
+
+            AddMap(6, DigitType.Zero, devices[5], 13);
+            AddMap(6, DigitType.One, devices[5], 11);
+            AddMap(6, DigitType.Two, devices[5], 1);
+            AddMap(6, DigitType.Three, devices[5], 6);
+            AddMap(6, DigitType.Four, devices[5], 4);
+            AddMap(6, DigitType.Five, devices[5], 3);
+            AddMap(6, DigitType.Six, devices[5], 2);
+            AddMap(6, DigitType.Seven, devices[5], 12);
+            AddMap(6, DigitType.Eight, devices[5], 15);
+            AddMap(6, DigitType.Nine, devices[5], 14);
+            AddMap(6, DigitType.RightDot, devices[5], 0);
+            AddMap(6, DigitType.LeftDot, devices[5], 5);
+
+        }
+
+        private void AddMap(int tube, DigitType digit, Pca9685 device, int channel)
+        {
+            devicesMap.Add((tube, (int)digit), (device, channel));
         }
 
         public void Render(Board board)
         {
-            if (this.RenderToConsole)
-                Console.Clear();
             for (int i = 0; i < board.Tubes.Length; i++)
             {
                 var tube = board.Tubes[i];
-                if (this.RenderToConsole)
-                    ToConsole(i, tube.Digits);
-                //for (int j = 0; j < tube.Digits.Length; j++)
-                //{
-                //    var digit = tube.Digits[j];
-                //}
+                if (ToConsole) WriteToConsole(i, tube.Digits);
+
+
+                for (int j = 0; j < tube.Digits.Length; j++)
+                {
+                    var digit = tube.Digits[j];
+                    if (digit.DutyCycle != currentState.Tubes[i].Digits[j].DutyCycle)
+                    {
+                        currentState.Tubes[i].Digits[j].DutyCycle = digit.DutyCycle;
+                        (Pca9685 device, int channel) = devicesMap[(i, j)];
+                        device.SetDutyCycle(channel, digit.DutyCycle);
+                    }
+                }
             }
         }
 
-        private void ToConsole(int tube, Digit[] digits)
+        private void WriteToConsole(int tube, Digit[] digits)
         {
-
             StringBuilder builder = new StringBuilder();
             builder.Append("T");
             builder.Append(tube);
--- src/csharp/nixi-clock/Program.cs
+++ src/csharp/nixi-clock/Program.cs
@@ -9,59 +9,81 @@ namespace nixi_clock
 {
     class Program
     {
+        //Package Mangager Console: & "C:\Program Files\PuTTY\pscp.exe" -r -i C:\users\mrjav\.ssh\ssh-key.ppk C:\Users\mrjav\Documents\nixi-clock-3\src\csharp\nixi-clock\bin\Debug\netcoreapp3.0\* richard@192.168.1.7:/home/richard/nixi-clock-debug
+        //Visual Studio Command Window: DebugAdapterHost.Launch /LaunchJson:C:\Users\mrjav\Documents\nixi-clock-3\src\csharp\nixi-clock\Properties/remoteLaunch.json /EngineGuid:541B8A8A-6081-4506-9F0A-1CE771DEBC04
+
         const string BUS = "/dev/i2c-0";
         static void Main(string[] args)
         {
-
-            //I2cDevice device = I2cDevice.Create(new I2cConnectionSettings(0, Pca9685.I2cAddressBase));
-            //Pca9685 board = new Pca9685(device);
+            //TestMap();
+            //Console.WriteLine("Done");
             BoardRenderer renderer = new BoardRenderer();
-            renderer.RenderToConsole = true;
+            //renderer.RenderToConsole = true;
             Clock clock = new Clock();
-            while(true)
+            Board currentState = new Board();
+            while (true)
             {
                 Board board = clock.GetBoard();
-                renderer.Render(board);
-                Thread.Sleep(100);
+                currentState.Interpolate(board, 0.15f);
+                renderer.Render(currentState);
+                Thread.Sleep(10);
             }
-
-            ////Opens bus
-            //int fd;
-            //if ((fd = I2C.open_bus(Encoding.UTF8.GetBytes(BUS))) < 0)
-            //{
-            //    Console.WriteLine("Failed to open bus! %d\n", fd);
-            //    return;
-            //}
-            //if (Tubes.init_tubes(fd) != 0)
-            //{
-            //    Console.WriteLine("Failed to init the tubes! %d\n", fd);
-            //    return;
-            //}
-
-            //            TubesController tubesController = new TubesController(fd);
-            //            tubesController.Run();
-            //            TestTubes(fd);
-            //Tubes.set_tube(fd, 0, 5, (byte)255);
-            //I2C.close_bus(fd);
         }
 
-        private static void TestTubes(int fd)
+        private static void TestMap()
         {
-            int brightness, tube, digit = 0;
-            for (tube = 0; tube < 7; tube++)
+            BoardRenderer boardRenderer = new BoardRenderer();
+            for (int tube = 0; tube < 7; tube++)
             {
-                for (digit = 0; digit < 12; digit++)
+                for (int digit = 0; digit < 12; digit++)
                 {
-                    for (brightness = 1; brightness <= 0x09; brightness++)
+                    Console.WriteLine($"Tube: {tube}, Digit: {digit}");
+                    for (int i = 0; i < 100; i++)
                     {
-                        //Tubes.set_tube(fd, tube, digit, (byte)brightness);
-                        Thread.Sleep(33);
+                        var board = new Board();
+                        board.Tubes[tube].Digits[digit].DutyCycle = i / 100.0;
+                        boardRenderer.Render(board);
+                        Thread.Sleep(1);
                     }
-                    for (brightness = 0x08; brightness >= 0; brightness--)
+                    for (int i = 100; i > 0; i--)
                     {
-                        //Tubes.set_tube(fd, tube, digit, (byte)brightness);
-                        Thread.Sleep(33);
+                        var board = new Board();
+                        board.Tubes[tube].Digits[digit].DutyCycle = i / 100.0;
+                        boardRenderer.Render(board);
+                        Thread.Sleep(1);
                     }
+                    boardRenderer.Render(new Board());
+                }
+            }
+        }
+
+
+        private static void TestTubes()
+        {
+            Console.WriteLine("Connecting to devices");
+            Pca9685[] devices = new Pca9685[6];
+            for (int i = 0; i < devices.Length; i++)
+                devices[i] = new Pca9685(I2cDevice.Create(new I2cConnectionSettings(0, Pca9685.I2cAddressBase + i)), 1000);
+
+            Console.WriteLine("Init...");
+            Array.ForEach(devices, d => d.SetDutyCycleAllChannels(0));
+            Console.WriteLine("Tests all channels on the devices");
+            for (int device = 0; device < devices.Length; device++)
+            {
+                for (int channel = 0; channel < 16; channel++)
+                {
+                    Console.WriteLine($"Device: {device}, Channel: {channel}");
+                    for (int i = 0; i < 100; i++)
+                    {
+                        devices[device].SetDutyCycle(channel, i / 100.0);
+                        Thread.Sleep(1);
+                    }
+                    for (int i = 100; i > 0; i--)
+                    {
+                        devices[device].SetDutyCycle(channel, i / 100.0);
+                        Thread.Sleep(1);
+                    }
+                    devices[device].SetDutyCycle(channel, 0);
                 }
             }
         }
